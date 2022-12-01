0d66073a45d538a456d7f9ac684026fe054bcfc0
Sergii Podriezienko <podreenko1986@gmail.com>
2022-12-01 19:04:01 +0200

    menu
    menu

---
+++ script/menu.js
@@ -0,0 +1,14 @@
+(() => {
+
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+
+    menuBtnRef.addEventListener("click", () => 
+    {
+    
+      menuBtnRef.classList.toggle("is-open");
+  
+    });
+
+
+  }) ();
+  
